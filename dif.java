9635757f377517c4a1e6e8f829896a07f627c336
Roopareddy28 <bujjireddyj@gmail.com>
2023-03-31 19:49:36 +0530

    adding dif.java

---
+++ dif.java
@@ -0,0 +1,12 @@
+package Learngit;
+
+public class dif {
+    
+   public static void main(String[] args)
+   {
+    int a=7;
+    int b=6;
+    System.out.println("The difference of two numbers is:" +(a-b));
+   }
+    
+}
