d541e471cdb9c81ac57aa52eb43d7e67961fc1d8
Roopareddy28 <bujjireddyj@gmail.com>
2023-03-31 20:02:01 +0530

    added multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+package Learngit;
+
+public class Multiply {
+    public static void main(String[] args)
+    {
+     int a=5;
+     int b=6;
+     System.out.println("The sum of two numbers is:" +(a*b));
+    }  
+    
+}
